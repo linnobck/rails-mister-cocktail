2e90a2fcf41b6b1c4bff59c25550308399dd8770
Linn Oberbeck <linnoberbeck@gmail.com>
2019-05-17 16:17:10 +0200

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Dose.destroy_all
+Ingredient.destroy_all
+
+Ingredient.create(name: " Cucumber")
+Ingredient.create(name: " Gin")
+Ingredient.create(name: " Ginger beer")
+Ingredient.create(name: " Ice")
+Ingredient.create(name: " Lemon")
+Ingredient.create(name: " Mint leaves")
+Ingredient.create(name: " Vodka")
+Ingredient.create(name: " Tonic")
+Ingredient.create(name: " Sugar syrup")
+Ingredient.create(name: " Sparkling water")
+Ingredient.create(name: " Lime")
+Ingredient.create(name: " Rum")
+Ingredient.create(name: " Tequila")
+Ingredient.create(name: " Sekt")
+Ingredient.create(name: " Sour Mix")
+
